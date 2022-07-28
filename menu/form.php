17e5d5e73a303954211aad9dad140b36961adf96
Juan P Rodriguez <juannpe01@gmail.com>
2022-07-28 14:48:02 -0300

    Fin del proyecto(por ahora)

--- menu/form.php
+++ menu/form.php
@@ -7,14 +7,14 @@ $mensaje = $_POST['text'];
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje = "El mail de la persona es  " . $email . ",\r\n";
 $mensaje = "El mensaje es " . $mensaje . ",\r\n";
-$mensaje = "Enviado el  " . date('d/m/Y', time());
 
 $para = 'juannpe01@gmail.com';
 $asunto = 'Este mail fue enviado desde la web personal';
 
+header('Location:exito.html');
+
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header('Location:exito.html');
 
 
 ?>
